f6bf8e60379f36d73b6e2598ba92b5217f1bcbc5
torchdb <34298824@qq.com>
2024-08-30 21:43:08 +0800

    add umirc

---
+++ .umirc.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "@umijs/max";
+
+export default defineConfig({
+  title: "Template App",
+  // outputPath: "",
+  base: "/template/", 
+  publicPath: "/template/", // end with '/'
+  routes: [
+    { path: "/", component: "index" }
+  ],
+  antd: {
+    theme: {
+      token: {
+        colorPrimary: "#666666",
+        colorTextBase: "#444444",
+        colorLink: "#1890ff",
+        // borderRadius: 2
+      },
+      components: {
+        FloatButton: {
+          // colorPrimary: '#C34582FF',
+          colorTextBase: "#444444",
+        },
+      },
+    },
+  },
+  access: {},
+  model: {},
+  initialState: {},
+  request: {
+    dataField: "data",
+  },
+  codeSplitting: {
+    // 加速JS访问
+    jsStrategy: "granularChunks",
+  },
+  esbuildMinifyIIFE: true,
+  proxy: {
+    "/api/v1": {
+      target: "http://localhost:8080/",
+      changeOrigin: true,
+      pathRewrite: { "^/api/v1": "api/v1" },
+    },
+  },
+  npmClient: 'pnpm',
+});
